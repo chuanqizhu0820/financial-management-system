fad5508e52d8d806bdba54ebea2957a4e97d82d2
chuanqizhu0820 <chuanqizhu1991@outlook.com>
2021-10-01 14:27:23 +0100

    Save form date in mongodb

--- server.js
+++ server.js
@@ -9,11 +9,30 @@ app.use(express.urlencoded({
 let uri = "mongodb+srv://dbUser:dbUserpw@cluster0.qjxut.mongodb.net/db2021?retryWrites=true&w=majority"
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+const { Schema } = mongoose;
+const itemSchema = new Schema(
+    {
+        date: { type: Date, required: true },
+        amount: { type: Number, required: true },
+        category: { type: String, required: true },
+        note: String
+    }
+)
+
+let Item = mongoose.model("Item", itemSchema);
+
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 })
 
 app.post("/add", (req, res) => {
+    let item = new Item({
+        date: req.body.date,
+        amount: req.body.amount,
+        category: req.body.category,
+        note: req.body.note
+    })
+    item.save();
     res.json(req.body);
 })
 
